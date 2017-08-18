d133f3453a6eb0665f40cdb2085ea76676568a96
Philipp Hossner <philipp.hossner@posteo.de>
2017-08-18 13:48:22 +0200

    Bump version to 0.1.2 because of some PyPI hiccups

--- django_mutpy/__init__.py
+++ django_mutpy/__init__.py
@@ -1,3 +1,3 @@
 """This is the top level package of django-mutpy."""
 
-__version__ = VERSION = (0, 1, 0)
+__version__ = VERSION = (0, 1, 2)
--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
 """Setup script."""
+from os import path
+
 from setuptools import setup, find_packages
 
 from django_mutpy import __version__
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='django-mutpy',
     version='.'.join(str(x) for x in __version__),
     license='Apache 2.0',
     description='Mutation testing with MutPy for Django.',
+    long_description=long_description,
     author='Philipp Hossner',
     author_email='philipp.hossner@posteo.de',
     url='https://github.com/phihos/django-mutpy',
