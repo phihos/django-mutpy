19f0c59cabc72cb18f95c5b9be0ba9620b75f203
Philipp Hossner <philipp.hossner@posteo.de>
2019-06-27 09:44:24 +0200

    Drop Python 3.3 support

--- setup.py
+++ setup.py
@@ -30,7 +30,6 @@ setup(
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
