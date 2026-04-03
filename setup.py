f6ed91fac68ce011eb761fd71c6d7813ba3b7e89
Philipp Hossner <phil@deepl.com>
2026-04-04 01:18:30 +0200

    Limit Python versions to 3.9-3.11 due to mutpy incompatibility
    MutPy 0.6.1 uses importlib.find_loader which was removed in Python
    3.12. Until mutpy is updated to support newer Python versions, the
    test matrix and classifiers are limited to 3.9-3.11.

--- setup.py
+++ setup.py
@@ -34,8 +34,6 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
-        'Programming Language :: Python :: 3.13',
         'Framework :: Django',
     ]
 )
